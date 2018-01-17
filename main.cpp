d7ccc68fe7a6b72deb1d68a6afdab6ed77e0af49
longago <1310421753@qq.com>
2018-01-17 22:12:55 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ char line[850],a[80],b[80];;
 
 vector<string> my_split(string line, char d=',') // string d=",")
 {
+    if (*line.rbegin()=='\n') line.resize(line.size()-1);
     vector<string> v;
     istringstream ss( line );
     while (!ss.eof())         // See the WARNING above for WHY we're doing this!
@@ -36,17 +37,33 @@ void get_first_lines()
 {
     double t0 = clock();
     FILE *fin = fopen("LBSN + keyword\\gowalla\\gowalla_spots_subset1.csv","r");//,stdin);
-    FILE *fout = fopen("LBSN + keyword\\gowalla\\gowalla_spots_subset1_200000.csv","w");//,stdout);
-  //  string line;
- //   cin.getline(line,500);
- prt(fin);prt(fout);
+    FILE *fout = fopen("LBSN + keyword\\gowalla\\gowalla_spots_cleaned_800000.csv","w");//,stdout);
+  //  prt(fin);prt(fout);
     fgets(line,800,fin);
     prt(line);
-
-    for (int i=0;i<200000;i++) {
+    for (int i=0;i<800000;i++) {
         fgets(line,800,fin);
-        if (i>10 && i<20) prt(line);
-        fputs(line,fout);
+        vector<string> vs = my_split(line);
+        if (vs.size()<=1) continue;
+        string id,lng,lat,cat;
+        id = vs[0], lng = vs[2], lat = vs[3], cat = *vs.rbegin();
+        string pat = "'name': "
+        int pos = cat.find(pat);
+        cat = cat.substr(pos+pat.size());
+        auto it = vs.rbegin();
+        it++;
+        string s = *it;
+        s.resize(s.size()-1);
+        vector<string> vs2 = my_split(cat,'\'');
+        assert(vs2.size()>=2);
+        it = vs2.rbegin();
+        it++;
+        cat = *it;
+        if ( i<15) {
+            prt(cat);
+        }
+        string newline = id+","+lng+","+lat+","+cat+"\n";;
+        fputs(newline.c_str(),fout);
     }
     fclose(fin);
     fclose(fout);
@@ -81,17 +98,17 @@ int str2int(string s)
     ss >> d;
     return d;
 }
-void demo1()
+void test_split()
 {
-    char* str = "ads,dasf,ikl,,gdf,";
+    char* str = "ads,dasf,ik l,,gdf,";
     vector<string> vs = my_split(str);
     prt(vs.size());
     for (auto s:vs) prt(s);
     prt(*vs.rbegin());
     prt(str);
 }
-map<int,set<int> > u2p;
-void get_user_labels()
+ map<int,set<int> >  u2p;
+void get_ul()
 {
     double t0 = clock();
     freopen("LBSN + keyword\\gowalla\\gowalla_checkins_200000.csv","r",stdin);
@@ -128,16 +145,73 @@ void get_user_labels()
     double t1 = clock();
     cerr << "Read time: " << (t1-t0)/CLOCKS_PER_SEC<<endl;
 }
-void demo2()
+
+map<int, string> spot;
+map<int, map<string,int> > ul;//[2688969 + 20];
+void process()
 {
-    char s[5];
-    gets(s);
-    puts(s);
-    prt(s);
+    FILE *fspot = fopen("LBSN + keyword\\gowalla\\gowalla_spots_subset0_800000.csv","r");
+    FILE *fci = fopen("LBSN + keyword\\gowalla\\gowalla_checkins_200000.csv","r");
+    assert(fspot!=NULL);
+    assert (fci!=NULL);
+    FILE *fo = fopen("LBSN + keyword\\gowalla\\gowalla_userlabel.csv","w");
+    int i = 0;
+    while (fgets(line,700,fspot)) {
+        vector<string> ss = my_split(line);
+        if (ss.size()<4) continue;
+        spot[str2int(ss[0])] = ss[3];
+        if (++i<4) prt(ss[3]);
+    }
+    i = 0;
+    while (fgets(line,700,fci)) { // check-in
+        vector<string> vs = my_split(line);
+        if (vs.size()<3) continue;
+        int u = str2int(vs[0]);
+        int p = str2int(vs[1]);
+
+        if (spot.count(p)==0) continue;
+        string label = spot[p];
+        if (++i<10) {
+            prt(u); prt(p);prt(label);
+        }
+
+        if (ul.count(u)==0) ul[u] = map<string,int>();
+      //  map<string,int> mp = &ul[user];
+        if (ul[u].count(label)==0) ul[u][label] = 0;
+        ul[u][label]++;
+    }
+    i = 0;
+    for (auto pp: ul) {
+        auto mp = pp.second;
+
+        fprintf(fo,"%d,%d",pp.first, mp.size());
+        if (++i<5) {
+            prt(pp.first);prt(mp.size());
+        }
+        int sum = 0;
+        for (auto s:mp) {
+            fprintf(fo,",%s,%d",s.first.c_str(),s.second);
+            sum += s.second;
+            if (i<=1) {
+                prt(s.first);prt(s.second);
+            }
+
+        }
+        fprintf(fo,",%d",sum);
+        if (i<3) cerr << endl;
+        fprintf(fo,"\n");
+    }
 }
+
 int main(int argc, char **argv)
 {
-    get_first_lines();
+//    stringstream ss("asdf,sdaf dsf");
+//    string s;
+//    while (ss>>s) {
+//        prt(s);
+//    }
+//    ss.str
+    process();
     return 0;
 }
 // intersect(): 13 35  53s
@@ -161,6 +235,21 @@ number_of_edges() = 88234
 
 Process returned 0 (0x0)   execution time : 61.621 s
 Press any key to continue.
+----------------
+u = 1338
+p = 482954
+label = Bar
 
+u = 1338
+p = 580963
+label = Gas & Automotive
+
+u = 1338
+p = 365256
+label = Stadium
+
+u = 1338
+p = 89504
+label = Zoo
 
 */
